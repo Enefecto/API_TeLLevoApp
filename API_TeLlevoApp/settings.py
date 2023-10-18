4b6858ca5446e8d8779b1a8f41e5668c7b92c9a6
Camilo Romero <camids980@gmail.com>
2023-10-18 10:20:57 -0300

    Settins.py UPDATED (Corsheaders)

--- API_TeLlevoApp/settings.py
+++ API_TeLlevoApp/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -49,6 +50,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200',  # Agrega aquí los dominios que quieres permitir
 ]
 
 ROOT_URLCONF = 'API_TeLlevoApp.urls'
